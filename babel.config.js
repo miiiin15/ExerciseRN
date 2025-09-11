e2742b70de18fce3586f568f8fb50c50da1451e3
mintae <gg04253@gmail.com>
2025-09-11 11:21:00 +0900

    chore: babel 포맷 변경

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+  ],
 };
